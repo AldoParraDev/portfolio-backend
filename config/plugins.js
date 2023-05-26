802a6f5df362fddc06fad6df4e737a679a3bb414
Aldo Parra <aldo.parra.dev@gmail.com>
2023-05-25 21:12:43 -0500

    first version of the finished app

---
+++ config/plugins.js
@@ -0,0 +1,66 @@
+module.exports = ({ env }) => ({
+  "strapi-plugin-populate-deep": {
+    config: {
+      defaultDepth: 10, // Default is 5
+    },
+  },
+  transformer: {
+    enabled: true,
+    config: {
+      prefix: "/api/",
+      responseTransforms: {
+        removeAttributesKey: true,
+        removeDataKey: true,
+      },
+    },
+  },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: env("SMTP_HOST"),
+        port: env("SMTP_PORT"),
+        auth: {
+          user: env("SMTP_USERNAME"),
+          pass: env("SMTP_PASSWORD"),
+        },
+      },
+      settings: {
+        defaultFrom: env("SMTP_EMAIL"),
+        defaultReplyTo: env("SMTP_EMAIL"),
+      },
+    },
+  },
+  ezforms: {
+    config: {
+      enableFormName: true,
+      captchaProvider: {
+        name: "none",
+      },
+      notificationProviders: [
+        {
+          name: "email",
+          enabled: true,
+          config: {
+            from: env("SMTP_EMAIL"),
+          },
+        },
+      ],
+    },
+  },
+});
